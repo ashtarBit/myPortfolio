0aa10ca6546dca312ae6cedc81788035f7f00061
ashtarBit <abdullahashtar6@gmail.com>
2024-10-11 11:56:26 +0200

    Updated script.js, added resumes in assets folder, and removed Personal-Photo.jpg

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function showResume(language) {
-    var resumeViewer = document.getElementById("resume-viewer");
-    var resumePdf = document.getElementById("resume-pdf");
+  var resumeViewer = document.getElementById("resume-viewer");
+  var resumePdf = document.getElementById("resume-pdf");
 
-    resumeViewer.style.display = "block";
+  resumeViewer.style.display = "block";
 
-    if (language === "english") {
-      resumePdf.src =
-        "https://drive.google.com/file/d/17iNEzUAAHVL-FOJJCHFAijrPYgzX7ax-/preview";
-    } else if (language === "german") {
-      resumePdf.src =
-        "https://drive.google.com/file/d/1bd69Fx9tB-jS2TS5GjT0irsEN7UJTkLi/preview";
-    }
+  if (language === "english") {
+    resumePdf.src = "https://github.com/your-username/your-repository/raw/main/assets/Curriculum Vitae.pdf";
+  } else if (language === "german") {
+    resumePdf.src = "https://github.com/your-username/your-repository/raw/main/assets/Lebenslauf.pdf";
   }
+}
